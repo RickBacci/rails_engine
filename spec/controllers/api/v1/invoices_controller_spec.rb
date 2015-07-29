a5a45dffc2bc0ca3d361970e940ddcfd1acecbad
Rick Bacci <rickbacci@gmail.com>
2015-07-29 13:15:16 -0600

    remove un-needed routes. Fix test.

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -5,23 +5,21 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
   describe "GET #random" do
     it "returns a random invoice" do
       20.times do |i|
-        Invoice.create(status: "shipped#{c}")
+        Invoice.create(status: "shipped#{i}")
       end
 
-      duplicate_ivoices = 0
+      duplicate_invoices = 0
 
       40.times do
         get :random, format: :json
-        c1 = (JSON.parse(response.body)['status'])
+        i1 = (JSON.parse(response.body)['status'])
         get :random, format: :json
-        c2 = (JSON.parse(response.body)['status'])
+        i2 = (JSON.parse(response.body)['status'])
 
         duplicate_invoices += 1 if i1 == i2
       end
 
-
-
-      expect(duplicate_ivoices).to be < 10
+      expect(duplicate_invoices).to be < 10
       expect(response).to have_http_status(:success)
     end
   end
@@ -42,51 +40,50 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
 
       get :find, id: invoice.id, format: :json
 
-      expect(JSON.parse(response.body)['status']).to eq('shiped')
+      expect(JSON.parse(response.body)['status']).to eq('shipped')
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #find_all" do
-    it "returns http success" do
+    xit "returns http success" do
       get :find_all
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #transactions" do
-    it "returns http success" do
+    xit "returns http success" do
       get :transactions
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #invoice_items" do
-    it "returns http success" do
+   xit "returns http success" do
       get :invoice_items
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #items" do
-    it "returns http success" do
+    xit "returns http success" do
       get :items
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #customer" do
-    it "returns http success" do
+    xit "returns http success" do
       get :customer
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #merchant" do
-    it "returns http success" do
+    xit "returns http success" do
       get :merchant
       expect(response).to have_http_status(:success)
     end
   end
-
 end
